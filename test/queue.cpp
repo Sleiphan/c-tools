7ffd7d286ce7d3e738516ddaebca8488eafb058f
Håkon F. Fjellanger <hfredrif@gmail.com>
2026-03-28 20:16:01 +0000

    Remove array pushing and popping from queue implementation

--- test/queue.cpp
+++ test/queue.cpp
@@ -19,42 +19,7 @@ extern "C" {
     #include <errno.h>
 }
 
-// TEST(queue, base_case) {
-//     int input_numbers[] = {0, 1, 2, 3, 4, 5};
-//     const int BUFFER_SIZE = sizeof(input_numbers) / sizeof(input_numbers[0]);
-//     int output_numbers[BUFFER_SIZE];
 
-//     memset(output_numbers, 0, sizeof(output_numbers));
-
-//     queue* q = queue_create();
-
-//     for (int i = 0; i < BUFFER_SIZE; i++)
-//         queue_push(q, (void*)&input_numbers[i]);
-    
-//     for (int i = 0; i < BUFFER_SIZE; i++)
-//         output_numbers[i] = *((int*)queue_pop(q));
-    
-//     queue_destroy(q);
-
-//     for (int i = 0; i < BUFFER_SIZE; i++)
-//         EXPECT_EQ(input_numbers[i], output_numbers[i]);
-// }
-
-// TEST(queue, emptying_and_reusing) {
-//     const int test_value_1 = 42;
-//     const int test_value_2 = 420;
-    
-//     queue* q = queue_create();
-
-//     for (int i = 0; i < 10; i++) {
-//         queue_push(q, (void*)&test_value_1);
-//         queue_push(q, (void*)&test_value_2);
-//         EXPECT_EQ(*((int*)queue_pop(q)), test_value_1);
-//         EXPECT_EQ(*((int*)queue_pop(q)), test_value_2);
-//     }
-    
-//     queue_destroy(q);
-// }
 
 TEST(queue, mask_is_created_correctly) {
     queue* q = queue_create(7);
@@ -135,6 +100,8 @@ TEST(queue, base_case) {
     queue_destroy(q);
 }
 
+
+
 TEST(queue, size_reports_correctly) {
     constexpr QUEUE_INDEX capacity = queue_max_size - 1;
     constexpr QUEUE_INDEX half_capacity = capacity / 2;
@@ -165,44 +132,6 @@ TEST(queue, size_reports_correctly) {
         queue_pop(q, &sink);
         EXPECT_EQ(queue_size(q), i - 1);
     }
-
-    // Verify that no errors occurred
-    EXPECT_EQ(errno, 0);
-    
-    queue_destroy(q);
-}
-
-TEST(queue, base_case_array) {
-    constexpr QUEUE_INDEX capacity = queue_max_size - 1;
-    constexpr QUEUE_INDEX half_capacity = capacity / 2;
-
-    queue* q = queue_create(capacity);
-
-
-
-    QUEUE_TYPE values[capacity], result[capacity];
-    for (QUEUE_TYPE i = 0; i < capacity; i++)
-        values[i] = i;
-    memset(result, 0, capacity * sizeof(QUEUE_TYPE));
-
-    int elements_pushed = queue_push_array(q, values, half_capacity);
-    int elements_popped = queue_pop_array(q, result, half_capacity);
-
-    EXPECT_TRUE(0 == memcmp(result, values, half_capacity * sizeof(QUEUE_TYPE)));
-    EXPECT_EQ(elements_pushed, half_capacity);
-    EXPECT_EQ(elements_popped, half_capacity);
-    memset(result, 0, half_capacity * sizeof(QUEUE_TYPE));
-
-
-
-    elements_pushed = queue_push_array(q, values, capacity);
-    elements_popped = queue_pop_array(q, result, capacity);
-
-    EXPECT_TRUE(0 == memcmp(result, values, capacity * sizeof(QUEUE_TYPE)));
-    EXPECT_EQ(elements_pushed, capacity);
-    EXPECT_EQ(elements_popped, capacity);
-
-
 
     // Verify that no errors occurred
     EXPECT_EQ(errno, 0);
